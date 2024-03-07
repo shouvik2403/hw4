2078b5eb2d3688d27af8787449073f97136b5db7
Shouvik Chaudhury <shouvik.chaudhury@kellogg.northwestern.edu>
2024-03-07 02:18:29 +0000

    commit 2

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,31 +4,26 @@ class SessionsController < ApplicationController
 
   def create
     # Authenticate user
-    # Ensure that email and password is not blank
-    if params["email"] != "" && params["password"] != ""
-      # If not blank, first, try to find user using their unique identifier, i.e. email
-      @user = User.find_by({"email" => params["email"]})
-      if @user != nil
-        # if user exists, check if they know their password
-        if BCrypt::Password.new(@user["password"]) == params["password"]
-          # add a secure cookie for this user
-          session["user_id"] = @user["id"]  
-          # display welcome message
-          flash["notice"] = "Welcome #{@user["first_name"]}"
-          redirect_to "/places"
-        else
-          flash["notice"] = "Sorry, wrong password!"
-          redirect_to "/login"
-        end  
+    # First, try to find user using their unique identifier, i.e. email
+    @user = User.find_by({"email" => params["email"]})
+    if @user != nil
+      # if user exists, check if they know their password
+      if BCrypt::Password.new(@user["password"]) == params["password"]
+        # add a secure cookie for this user
+        session["user_id"] = @user["id"]  
+        # display welcome message
+        flash["notice"] = "Welcome #{@user["first_name"]}"
+        redirect_to "/places"
       else
-        flash["notice"] = "This email does not exist."
-        redirect_to "/login"    
-      end
+        flash["notice"] = "Sorry, wrong password!"
+        redirect_to "/login"
+      end  
     else
-      flash["notice"] = "Email or password cannot be blank."
-      redirect_to "/login"
-    end      
+      flash["notice"] = "This email does not exist."
+      redirect_to "/login"    
+    end
   end
+  
 
   def destroy
     # logout the user
@@ -36,5 +31,6 @@ class SessionsController < ApplicationController
     flash["notice"] = "Goodbye!"
     redirect_to "/login"
   end
+
 end
   
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,28 +4,23 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new
-    @user["first_name"] = params["first_name"]
-    @user["last_name"] = params["last_name"]
+    @user["username"] = params["username"]
     @user["email"] = params["email"]
     @user["password"] = params["password"]
     
-    if @user["email"] == "" || @user["password"] == ""
-      flash["notice"] = "Email or Password cannot be blank!"
+    # check if the username exists
+    if User.find_by({"email" => @user["email"]}) == nil
+      # encrypt user's password "at rest"  
+      @user["password"] = BCrypt::Password.create(params["password"])
+      @user.save
+      # add a secure cookie for this user
+      session["user_id"] = User.find_by({"email" => @user["email"]})["id"]  
+      # redirect to places page
+      redirect_to "/places"
+    else   
+      flash["notice"] = "An account with this email already exists! Please try a different email or login to your account."
       redirect_to "/users/new"
-    else
-      if User.find_by({"email" => @user["email"]}) == nil
-        # encrypt user's password "at rest"  
-        @user["password"] = BCrypt::Password.create(params["password"])
-        @user.save
-        # add a secure cookie for this user
-        session["user_id"] = User.find_by({"email" => @user["email"]})["id"]  
-        # redirect to places page
-        redirect_to "/places"
-      else   
-        flash["notice"] = "An account with this email already exists! Please try a different email or login to your account."
-        redirect_to "/users/new"
-      end
-    end  
-  end
+    end
+  end  
 
 end
